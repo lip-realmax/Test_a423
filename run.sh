812ddc75308f229d7ed2cc8f5d8428e52ed56100
lip-realmax <56542376+lip-realmax@users.noreply.github.com>
2020-01-22 13:04:48 +0800

    Update for SSH access issue

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
-cat pack.a* > pack.tar.gz
-tar -zxf pack.tar.gz
-rm pack.*
-chmod +x pack/multi_hand_tracking_gpu
+if [ ! -d "pack" ]; then
+  cat pack.a* > pack.tar.gz
+  tar -zxf pack.tar.gz
+  rm pack.*
+  chmod +x pack/multi_hand_tracking_gpu
+fi
+export DISPLAY=:0
 cd pack
 ./multi_hand_tracking_gpu
